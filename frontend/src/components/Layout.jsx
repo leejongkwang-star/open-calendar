ecce8923d6f1143785c1d920d6be1b7d4acb8af7
LEEJONGKWANG-star <leejongkwang@gmail.com>
2026-01-09 10:59:31 +0900

    2.0.2.0 캘린더화면늘리기

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -116,7 +116,7 @@ function Layout() {
       </header>
 
       {/* 메인 컨텐츠 */}
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="max-w-7xl mx-auto px-2 sm:px-3 lg:px-4 py-4 sm:py-6">
         <Outlet />
       </main>
     </div>
